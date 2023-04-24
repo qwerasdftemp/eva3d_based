7e1ab4ef22e9dc3fc266b0f3ecc7462bfcaffc77
talegqz <gaoqingzhe97@gmail.com>
2023-04-24 17:43:13 +0000

    save

--- v19_part_uvd/scripts/2_gqz_debug_train.sh
+++ v19_part_uvd/scripts/2_gqz_debug_train.sh
@@ -2,4 +2,4 @@ MASTER_PORT=$((12000 + $RANDOM % 20000))
 NUM_GPU=$1
 set -x
 
-python -m torch.distributed.launch --nproc_per_node ${NUM_GPU} --master_port=${MASTER_PORT} train_deepfashion.py --batch 1 --chunk 1 --expname l300 --dataset_path /home/gqz/gqzwork/dataset/DeepFashion --depth 5 --width 128 --style_dim 512 --renderer_spatial_output_dim 512 256 --input_ch_views 3 --white_bg --r1 300 --voxhuman_name eva3d_deepfashion --random_flip --eikonal_lambda 0  --small_aug --iter 1000000 --adjust_gamma --gamma_lb 20 --min_surf_lambda 1 --deltasdf --gaussian_weighted_sampler --sampler_std 15 --N_samples 28 --glr 2e-3 
+python -m torch.distributed.launch --nproc_per_node ${NUM_GPU} --master_port=${MASTER_PORT} train_deepfashion.py --batch 1 --chunk 1 --expname l300 --dataset_path /home/gqz/gqzwork/dataset/DeepFashion --depth 5 --width 128 --style_dim 128 --renderer_spatial_output_dim 512 256 --input_ch_views 3 --white_bg --r1 300 --voxhuman_name eva3d_deepfashion --random_flip --eikonal_lambda 0  --small_aug --iter 1000000 --adjust_gamma --gamma_lb 20 --min_surf_lambda 1 --deltasdf --gaussian_weighted_sampler --sampler_std 15 --N_samples 28 --glr 2e-3 
